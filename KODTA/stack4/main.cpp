7f92b8f2378c2d280e1e0dc66c7cec1baa5790c7
starwispsora <142126413+starwispsora@users.noreply.github.com>
2024-07-29 17:02:09 +0900

    cpp class

--- KODTA/stack4/main.cpp
+++ KODTA/stack4/main.cpp
@@ -3,18 +3,22 @@
 
 int main()
 {
-    Stack s1;
-    Stack s2(20);
-
-    for (int i = 1; i <= 10; ++i) {
-        if (!s1.full()) {
-            s1.push(i * 100);
-        }
-    }
+    Stack<int> s1;
+    s1.push(100);
+    s1.push(200);
+    s1.push(300);
 
     while (!s1.empty()) {
         std::cout << s1.pop() << std::endl;
     }
 
+    Stack<double> s2;
+    s2.push(1.1);   
+    s2.push(2.2);
+    s2.push(3.3);
+
+    while (!s2.empty()) {
+        std::cout << s2.pop() << std::endl;
+    }
     return 0;
 }
--- KODTA/stack4/stack.h
+++ KODTA/stack4/stack.h
@@ -2,26 +2,99 @@
 #define STACK_H
 #include "array.h"
 
+templete <typename T>
 class Stack {
 private:
     static const int STACKSIZE; // Fixed typo here
     int *pArr_;
     int size_;
-    Array arr_; //has-a relationship
+    Array<T> arr_; //has-a relationship
     int tos_;
 
-    Stack(const Stack& rhs); // Copy constructor is private
-    Stack& operator=(const Stack& rhs); // Assignment operator is private
+    Stack(const Stack<T>& rhs); // Copy constructor is private
+    Stack<T>& operator=(const Stack<T>& rhs); // Assignment operator is private
 
 public:
     explicit Stack(int size = STACKSIZE); // Default constructor
     ~Stack(); // Destructor
 
-    void push(int data);
-    int pop();
+    void push(const T& data);
+    const T pop(); //why const? : Because it doesn't change the object
 
     bool full() const;
     bool empty() const;
 };
+
+#include <cassert>
+
+templete <typename T>
+const int Stack<T>::STACKSIZE = 100;
+
+templete <typename T>
+Stack<T>::Stack(int size)
+: arr_(size), tos_(0)
+{
+    
+}
+templete <typename T>
+Stack<T>::~Stack()
+{
+    //arr_.~Array(); // be called automatically
+}
+templete <typename T>
+void Stack<T>::push(int data)
+{
+    assert(!full());
+    arr_[tos_] = data;
+    ++tos_;
+}
+templete <typename T>
+const T Stack<T> ::pop()
+{
+    assert(!empty());
+    --tos_;
+    
+    return arr_[tos_]; // Fixed typo here
+}
+templete <typename T>
+bool Stack<T>::full() const
+{
+    
+    return tos_ == arr_.size();
+}
+templete <typename T>
+bool Stack<T>::empty() const
+{
+    return tos_ == 0;
+}
+
+templete <typename T>
+Stack<T>::Stack(const Stack<T>& rhs)
+: pArr_(nullptr), size_(rhs.size_), tos_(rhs.tos_)
+{
+    pArr_ = new int[size_];
+    assert(pArr_);
+    for (int i = 0; i < tos_; ++i)
+    {
+        pArr_[i] = rhs.pArr_[i];
+    }
+}
+templete <typename T>
+Stack& Stack<T>::operator=(const Stack<T>& rhs)
+{
+    if (this != &rhs) {
+        delete [] pArr_; 
+
+        size_ = rhs.size_;
+        tos_ = rhs.tos_;
+        pArr_ = new int[size_];
+        assert(pArr_);
+
+        for (int i = 0; i < tos_; ++i) {
+            pArr_[i] = rhs.pArr_[i];
+        }
+    }
+    return *this;
+}
 
 #endif
